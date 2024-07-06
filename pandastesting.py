ac76f3b140f3cd1f180b6676d1067f4bae6cc091
sanchit bhardwaj <sanchit.bhard@gmail.com>
2024-07-06 12:53:55 -0400

    adding csv file

---
+++ pandastesting.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+df1 = pd.read_csv('/Users/sanchitbhardwaj/PycharmProjects/DE_Pandas/costs.csv')
+print(df1)
+print(df1.shape)
+print(df1.columns)
+
+df2 = pd.read_csv('/Users/sanchitbhardwaj/PycharmProjects/DE_Pandas/costs.csv')
+print(df2)
+
+test_result = df1.compare(df2)
+print(test_result)  # there is no difference in the df1 and df2 means test result pass at location1 and location 2
+
+
+
---
+++ pytest.py
@@ -0,0 +1,8 @@
+
+
+def add(x):
+    return x+1
+    print(num)
+
+def test_answear():
+    assert add(3) ==5
