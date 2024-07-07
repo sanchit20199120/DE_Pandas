3a9e895207cfa109f275b38b0dedf1dc667ab2b8
sanchit bhardwaj <sanchit.bhard@gmail.com>
2024-07-07 13:26:01 -0400

    try, except and json.dump() and json.load() file concept

---
+++ test_name_function.py
@@ -0,0 +1,20 @@
+import unittest2
+from name_function import get_formatted_name
+
+
+class NameTestCase(unittest2.TestCase):
+    """Test's for name_function.py"""
+    def test_first_last_name(self):
+        formatted_name = get_formatted_name('sanchit','bhardwaj')
+        self.assertEqual(formatted_name, 'Sanchit Bhardwaj')
+
+    def test_first_middle_last_name(self):
+        formatted_name = get_formatted_name('sanchit','bhardwaj','kumar')
+        self.assertEqual(formatted_name, 'Sanchit Kumar Bhardwaj')
+
+
+
+if __name__ == '__main__':
+    unittest2.main()
+
+# run the file using terminal
